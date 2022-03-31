a2896583be57c06d2d77ae5d317aa4bbe4148935
alicegiacomelli <100956841+alicegiacomelli@users.noreply.github.com>
2022-03-31 16:09:49 +0200

    Update R_code_spectral_indices.r

--- R_code_spectral_indices.r
+++ R_code_spectral_indices.r
@@ -7,13 +7,13 @@ library(rgdal)
 setwd("/Users/alicegiacomelli/desktop/lab")
 
 # importazione immagine defor1_ e assegnazione
-l1922 <- brick("defor1_.jpg")
+l1992 <- brick("defor1_.jpg")
 
 # info 
-l1922
+l1992
 
 # plot RGB 
-plotRGB(l1922, r=1, g=2, b=3, stretch="lin")
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
 # layer 1 = NIR
 # layer 2 = red
 # layer 3 = green 
@@ -29,9 +29,37 @@ plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
 
 # multiframe l1992 e l2006
 par(mfrow=c(2,1))
-plotRGB(l1922, r=1, g=2, b=3, stretch="lin")
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
 plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
 
 # DVI Difference Vegetation Index 
 # differenza NIR e red 
-dvi1992 = l1922[[1]] - l1922[[2]]
+
+#1922
+dvi1992 = l1992[[1]] - l1992[[2]]
+
+# oppure
+# dvi1992 = l1992$defor1_.1 - l1992$defor1_.2
+
+# info 
+dvi1992
+
+# colorazione 
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) 
+plot(dvi1992, col=cl)
+
+#2006
+dvi2006 = l2006[[1]] - l2006[[2]]
+dvi2006
+
+# colorazione 
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) 
+plot(dvi2006, col=cl)
+
+# DVI differenza 1992 e 2006
+dvi_dif = dvi1992 - dvi2006
+cld <- colorRampPalette(c('blue','white','red'))(100)
+# chiudere multiframe 
+dev.off()
+# plot differenza DVI
+plot(dvi_dif, col=cld)
