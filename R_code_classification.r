dc62b93e97208ea3f43e46a3af4250ced19125ca
alicegiacomelli <100956841+alicegiacomelli@users.noreply.github.com>
2022-04-21 17:51:31 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -38,3 +38,53 @@ plot(soc$map, col=cl)
 # plot diversi anche se proviamo più volte sullo stesso pc 
 # funzione set.seed per avere sempre lo stesso esperimento 
 
+
+#### GRAND CANYON ####
+
+# importare immagine 
+# funzione brick ""
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+gc 
+
+# plot RGB 
+plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+# rosso=1
+# verde=2
+# blu=3
+
+# cambiare lo stretch in Histogram 
+# non tutti i valori sono riempiti e grazie allo strecht arrivo a 255, i valori si ridistribuiscono (questione solo di visualizzazione)
+# aumentare la capacità di visualizzazione con histogram 
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+
+#### classificare immagine ####
+# funzione unsuperClass
+
+# 2 classi
+# assegnazione
+gcclass2 <- unsuperClass(gc, nClasses=2)
+gcclass2
+# map è l'unico output 
+# plot legando classe e map 
+plot(gcclass2$map)
+# classe con 2 valori: 1 e 2. 
+# classe 1: roccia 
+# classe 2: acqua, ombre o altre tipologie di roccia 
+# vebgono scelti pixel random
+# per mantenere sempre la stessa classificazione 
+# set.seed(17)
+
+# classificazione con 4 classi 
+gcclass4 <- unsuperClass(gc, nClasses=4)
+gcclass4
+plot(gcclass4$map)
+# palette 
+clc <- colorRampPalette(c('yellow','red','blue','black'))(100)
+plot(gcclass4$map, col=clc)
+
+# comparare mappe con classi e immagine originale 
+par(mfrow=c(2,1))
+plot(gcclass4$map, col=clc)
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+
+
