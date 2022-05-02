b6f832d8594d2131b24e666118d119a2727558c4
alicegiacomelli <100956841+alicegiacomelli@users.noreply.github.com>
2022-05-02 16:10:01 +0200

    Create R_code_land_cover.r

---
+++ R_code_land_cover.r
@@ -0,0 +1,13 @@
+# creazione mappe land cover 
+
+library(raster)
+library(RStoolbox) # classification
+
+# set working directory 
+
+
+# install.packages("ggplot2")
+library(ggplot2)
+
+# install.packages("gridExtra")
+library(gridExtra)
